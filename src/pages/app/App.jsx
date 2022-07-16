5b38b88de502c54d70668aef03d17036c762a9c5
Alberto Araújo <albertoafaj@hotmail.com>
2022-07-16 18:31:19 -0300

    Refatorou o app para chamar a API com o clique de botão e atualizar as frases

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled  from "styled-components";
 import narutoImg from "../../images/naruto.png";
 import { Quotes } from "./../../components";
+import { getQuote } from '../../services';
 
 export function App(){
+
+    const [quoteState, setQuoteState] = useState({
+        quote: 'Ok',
+        speaker: ' Speaker'
+    });
+
+    const onUpdate = async () => {
+        const quote = await getQuote();
+        setQuoteState(quote);
+        
+    }
+
     return (
         <Content>
-            <Quotes quote={'ok'} speaker={'Speaker'}/>
+            <Quotes {...quoteState} onUpdate={onUpdate}/>
             <NarutoImg src={narutoImg} alt="Naruto with a kunai" />
         </Content>
 
--- src/pages/app/app.test.js
+++ src/pages/app/app.test.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { render, screen  } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { App } from "./App";
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const response = { speaker: 'Speaker', quote: 'test quote'};
+
+const server = setupServer(
+    rest.get('http://127.0.0.1:5000', (req,res,ctx)=> {
+      return res(ctx.json(response));  
+    })
+);
+
+beforeAll(()=> server.listen());
+afterEach(()=> server.resetHandlers());
+afterAll(()=> server.close());
 
 test('render the app, with a button, a quote and a button',  () => {
     render(<App /> );
-
+    
     const buttonEl = screen.getByRole('button');
     const imageEl = screen.getByRole('img');
     const textEl = screen.getByText(/Speaker/);
@@ -13,3 +27,14 @@ test('render the app, with a button, a quote and a button',  () => {
     expect(imageEl).toBeInTheDocument();
     expect(textEl).toBeInTheDocument();
 });
+
+
+test('calls api on button click and update its next', async ()=> {
+    render(<App /> );
+    const buttonEl = screen.getByRole('button');
+    fireEvent.click(buttonEl);
+    const quoteEl = await screen.findByText(response.quote);
+    expect(quoteEl).toBeInTheDocument();
+    
+    
+})
